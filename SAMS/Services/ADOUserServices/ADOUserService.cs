7ad1ab8d5022b7571aacc3575e24c0f63972c4b2
reset-egs <souleater12456@gmail.com>
2022-03-23 13:59:21 +0100

    Add - login functionality

---
+++ SAMS/Services/ADOUserServices/ADOUserService.cs
@@ -0,0 +1,21 @@
+﻿namespace SAMS.Services.ADOUserServices
+{
+    public class ADOUserService : IUserService
+    {
+        private ADOUser service;
+        public ADOUserService(ADOUser service)
+        {
+            this.service = service;
+        }
+
+        public User Login(string username, string password)
+        {
+            return service.Login(username, password);
+        }
+
+        public void SignUp(string username, string password, int studentNo)
+        {
+            service.SignUp(username, password, studentNo);
+        }
+    }
+}
